6d5dff8bfe395e19e786cccfd2890a9014c5cbaf
tomacht <thomas@flexio.fr>
2019-11-22 11:47:31 +0100

    fakenodeelement

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export {HotballoonTestDummy} from './src/js/HotballoonTestDummy'
 export {ApplicationWithStyle} from './src/js/ApplicationWithStyle'
+export {FakeNodeElement} from './src/js/FakeNodeElement'
