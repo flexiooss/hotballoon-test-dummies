619385e7c907d63f3bfff154b9ff4df7619ce6a4
tomacht <thomas@flexio.fr>
2019-12-20 16:59:32 +0100

    fakedocument

--- src/js/FakeDocument.js
+++ src/js/FakeDocument.js
@@ -1,3 +1,152 @@
 export class FakeDocument {
+  addEventListener() {
+    console.log('FakeDocument:addEventListener')
+  }
 
+  adoptNode() {
+  }
+
+  append() {
+  }
+
+  caretRangeFromPoint() {
+  }
+
+  clear() {
+  }
+
+  close() {
+  }
+
+  createAttribute() {
+  }
+
+  createCDATASection() {
+  }
+
+  createComment() {
+  }
+
+  createDocumentFragment() {
+  }
+
+  createElement() {
+  }
+
+  createElementNS() {
+  }
+
+  createEntityReference() {
+  }
+
+  createEvent() {
+  }
+
+  createExpression() {
+  }
+
+  createNodeIterator() {
+  }
+
+  createNSResolver() {
+  }
+
+  createProcessingInstruction() {
+  }
+
+  createRange() {
+  }
+
+  createTextNode() {
+  }
+
+  createTouch() {
+  }
+
+  createTouchList() {
+  }
+
+  createTreeWalker() {
+  }
+
+  enableStyleSheetsForSet() {
+  }
+
+  evaluate() {
+  }
+
+  execCommand() {
+  }
+
+  exitFullscreen() {
+  }
+
+  exitPointerLock() {
+  }
+
+  getAnimations() {
+  }
+
+  getBoxObjectFor() {
+  }
+
+  getElementById() {
+  }
+
+  getElementsByClassName() {
+  }
+
+  getElementsByName() {
+  }
+
+  getElementsByTagName() {
+  }
+
+  getElementsByTagNameNS() {
+  }
+
+  hasFocus() {
+  }
+
+  hasStorageAccess() {
+  }
+
+  importNode() {
+  }
+
+  mozSetImageElement() {
+  }
+
+  open() {
+  }
+
+  prepend() {
+  }
+
+  queryCommandEnabled() {
+  }
+
+  queryCommandSupported() {
+  }
+
+  querySelector() {
+  }
+
+  querySelectorAll() {
+  }
+
+  registerElement() {
+  }
+
+  releaseCapture() {
+  }
+
+  requestStorageAccess() {
+  }
+
+  write() {
+  }
+
+  writeln() {
+  }
 }
