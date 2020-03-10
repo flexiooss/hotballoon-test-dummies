8d19fe0c3f09e1238e241633ec86809d5aefc8b4
tomacht <thomas@flexio.fr>
2020-03-10 20:26:38 +0100

    implement generique fake requester

--- src/js/requester/FakeRequester.js
+++ src/js/requester/FakeRequester.js
@@ -1,45 +1,316 @@
-export class FakeHttpRequester {
+class RequestSendBuilder {
 
-  constructor(expectedResponseHeaders, expectedResponseCode, expectedResponseBody) {
+  constructor() {
+
+    /**
+     * @type {?Object}
+     * @private
+     */
+    this.__headers = null
+    /**
+     * @type {?string}
+     * @private
+     */
+    this.__path = null
+    /**
+     * @type {?Object}
+     * @private
+     */
+    this.__parameters = null
+    /**
+     * @type {?string}
+     * @private
+     */
+    this.__body = null
+    /**
+     * @type {?string}
+     * @private
+     */
+    this.__method = null
+  }
+
+  /**
+   * @param value
+   * @return {RequestSendBuilder}
+   */
+  headers(value) {
+    this.__headers = value
+    return this
+  }
+
+  /**
+   * @param value
+   * @return {RequestSendBuilder}
+   */
+  path(value) {
+    this.__path = value
+    return this
+  }
+
+  /**
+   * @param value
+   * @return {RequestSendBuilder}
+   */
+  parameters(value) {
+    this.__parameters = value
+    return this
+  }
+
+  /**
+   * @param value
+   * @return {RequestSendBuilder}
+   */
+  body(value) {
+    this.__body = value
+    return this
+  }
+
+  /**
+   * @param value
+   * @return {RequestSendBuilder}
+   */
+  method(value) {
+    this.__method = value
+    return this
+  }
+
+  /**
+   * @return {RequestSend}
+   */
+  build() {
+    return new RequestSend(
+      this.__headers,
+      this.__path,
+      this.__parameters,
+      this.__body,
+      this.__method
+    )
+  }
+}
+
+
+class RequestSend {
+  /**
+   *
+   * @return {Object}
+   */
+  headers() {
+    return this.__headers
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  path() {
+    return this.__path
+  }
+
+  /**
+   *
+   * @return {Object}
+   */
+  parameters() {
+    return this.__parameters
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  body() {
+    return this.__body
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  method() {
+    return this.__method
+  }
+
+  /**
+   *
+   * @param {Object} headers
+   * @param {string} path
+   * @param {Object} parameters
+   * @param {string} body
+   * @param {string} method
+   */
+  constructor(headers, path, parameters, body, method) {
+
+    /**
+     * @type {Object}
+     * @private
+     */
+    this.__headers = headers
+    /**
+     * @type {string}
+     * @private
+     */
+    this.__path = path
+    /**
+     * @type {Object}
+     * @private
+     */
+    this.__parameters = parameters
+    /**
+     * @type {string}
+     * @private
+     */
+    this.__body = body
+    /**
+     * @type {string}
+     * @private
+     */
+    this.__method = method
+  }
+}
+
+
+class FakeHttpRequester {
+  /**
+   *
+   * @param {?Object} [expectedResponseHeaders={}]
+   * @param {?Number} [expectedResponseCode=200]
+   * @param {?string} [expectedResponseBody={}]
+   * @param {?Number} [wait=0]
+   */
+  constructor(expectedResponseHeaders, expectedResponseCode = 200, expectedResponseBody = '{}', wait = 0) {
+    /**
+     * @type {Object}
+     * @private
+     */
     this.__expectedResponseHeaders = expectedResponseHeaders
+    /**
+     * @type {Number}
+     * @private
+     */
     this.__expectedResponseCode = expectedResponseCode
+    /**
+     * @type {string}
+     * @private
+     */
     this.__expectedResponseBody = expectedResponseBody
+    /**
+     * @type {Number}
+     * @private
+     */
+    this.__wait = wait
+
     this.__headers = {}
     this.__parameters = {}
     this.__path = null
     this.__lastMethod = null
+    this.__requestSend = null
+  }
+
+  /**
+   *
+   * @return {?RequestSend}
+   */
+  requestSend() {
+    return this.__requestSend
   }
 
   get(callback) {
-    this.__lastMethod = 'get'
-    callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+    this.__requestSend = new RequestSendBuilder()
+      .method('get')
+      .body(null)
+      .headers(this.__headers)
+      .parameters(this.__parameters)
+      .path(this.__path)
+      .build()
+      .setTimeout(() => {
+          callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+        },
+        this.__wait
+      )
   }
 
   delete(callback) {
-    this.__lastMethod = 'delete'
-    callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+    this.__requestSend = new RequestSendBuilder()
+      .method('delete')
+      .body(null)
+      .headers(this.__headers)
+      .parameters(this.__parameters)
+      .path(this.__path)
+      .build()
+
+    setTimeout(() => {
+        callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+      },
+      this.__wait
+    )
   }
 
   head(callback) {
+    this.__requestSend = new RequestSendBuilder()
+      .method('head')
+      .body(null)
+      .headers(this.__headers)
+      .parameters(this.__parameters)
+      .path(this.__path)
+      .build()
+
     this.__lastMethod = 'head'
-    callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+    setTimeout(() => {
+        callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+      },
+      this.__wait
+    )
   }
 
   post(callback, contentType = null, body = null) {
-    this.__lastMethod = 'post'
-    this.__lastBody = body
-    this.__lastContentType = contentType
-    callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+    this.__headers['Content-Type'] = contentType
+    this.__requestSend = new RequestSendBuilder()
+      .method('post')
+      .body(body)
+      .headers(this.__headers)
+      .parameters(this.__parameters)
+      .path(this.__path)
+      .build()
+
+    setTimeout(() => {
+        callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+      },
+      this.__wait
+    )
   }
 
-  put(callback, contentType, body) {
-    this.__lastMethod = 'put'
-    callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+  put(callback, contentType = null, body = null) {
+    this.__headers['Content-Type'] = contentType
+    this.__requestSend = new RequestSendBuilder()
+      .method('put')
+      .body(body)
+      .headers(this.__headers)
+      .parameters(this.__parameters)
+      .path(this.__path)
+      .build()
+
+    setTimeout(() => {
+        callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+      },
+      this.__wait
+    )
   }
 
-  patch(callback, contentType, body) {
-    this.__lastMethod = 'patch'
-    callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+  patch(callback, contentType = null, body = null) {
+    this.__headers['Content-Type'] = contentType
+    this.__requestSend = new RequestSendBuilder()
+      .method('patch')
+      .body(null)
+      .headers(this.__headers)
+      .parameters(this.__parameters)
+      .path(this.__path)
+      .build()
+
+    setTimeout(() => {
+        callback(new FakeResponseDelegate(this.__expectedResponseCode, this.__expectedResponseBody, this.__expectedResponseHeaders))
+      },
+      this.__wait
+    )
   }
 
   arrayParameter(name, values) {
@@ -75,6 +346,7 @@ export class FakeHttpRequesterBuilder {
     this.__expectedResponseBody = null
     this.__expectedResponseCode = null
     this.__expectedResponseHeaders = {}
+    this.__wait = null
   }
 
   /**
@@ -125,16 +397,31 @@ export class FakeHttpRequesterBuilder {
   }
 
   /**
+   * @description wait ms
+   * @param {number} value
+   * @return {FakeHttpRequesterBuilder}
+   */
+  wait(value) {
+    this.__wait = value
+    return this
+  }
+
+  /**
    *
    * @return {FakeHttpRequester}
    */
   build() {
-    return new FakeHttpRequester(this.__expectedResponseHeaders, this.__expectedResponseCode, this.__expectedResponseBody)
+    return new FakeHttpRequester(
+      this.__expectedResponseHeaders,
+      this.__expectedResponseCode,
+      this.__expectedResponseBody,
+      this.__wait
+    )
   }
 }
 
 
-export class FakeResponseDelegate {
+class FakeResponseDelegate {
 
   constructor(code, body, headers) {
     this.__body = body
